54bf6f88c72f5498ab4de9c7f87f3ae353687fb7
Igor Gato <igormqg@gmail.com>
2021-02-14 00:59:09 -0300

    Calculadora simples em HTML

---
+++ Calculadora Simples/README.md
@@ -0,0 +1,3 @@
+Calculadora simples (adição, subtração, multiplicação, divisão) utilizando HTML, CSS, E Javascript
+
+Previa do site: [Calculadora Simples](https://agitated-kare-b51058.netlify.app)
---
+++ Calculadora Simples/script.js
@@ -0,0 +1,32 @@
+
+let result = document.querySelector("#resultado")
+
+function soma1(){result.value+='1'}
+function soma2(){result.value+='2'}
+function soma3(){result.value+='3'}
+function soma4(){result.value+='4'}
+function soma5(){result.value+='5'}
+function soma6(){result.value+='6'}
+function soma7(){result.value+='7'}
+function soma8(){result.value+='8'}
+function soma9(){result.value+='9'}
+function soma0(){result.value+='0'}
+function ponto(){result.value+='.'}
+function adicao(){result.value+='+'}
+function subtracao(){result.value+='-'}
+function multiplicacao(){result.value+='*'}
+function divisao(){result.value+='/'}
+function apagar(){result.value=''}
+function resultado(){
+    try {
+        result.value=eval(result.value)
+    } catch (error) {
+        result.value ='Insira caracteres válidos'
+        setTimeout(function(){ result.value= ''} , 1300)
+    }
+        
+    }
+
+
+
+
